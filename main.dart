fc85fdabbece8d2913a16c3150962b3baeffefad
mariapaivadev <mariapaiva.dev@gmail.com>
2026-09-23 21:58:25 -0400

    Criando usuario main

--- main.dart
+++ main.dart
@@ -1,1 +1,24 @@
+import 'dart:io';
 
+import 'usuario.dart';
+import 'canal.dart';
+
+final usuarios = <usuario>[];
+final canais = <canal>[];
+
+string ler(String pergunta) {
+  print(pergunta);
+  return stdin.readLineSync() ?? '';
+}
+
+void criarUsuario({bool moderador = false}) {
+  final nick = ler('Digite o nick do usuário:');
+  final idade = int.tryParse(ler('Digite a idade do usuário:'));
+
+  usuarios.add(
+    moderador
+        ? moderador(nick: nick, idade: idade)
+        : usuario(nick: nick, idade: idade),
+  );
+  print('Criado com sucesso!');
+}
