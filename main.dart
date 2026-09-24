53e165a05079a916099aab1226712fd4cd44ade7
mariapaivadev <mariapaiva.dev@gmail.com>
2026-09-23 22:15:57 -0400

    Tentativa main.dart

--- main.dart
+++ main.dart
@@ -40,3 +40,68 @@ void enviarMensagem() {
   }
   c.adicionarMensagem(mensagem(u, ler('Mensagem:')));
 }
+
+void exibirCanal() {
+  final nome = ler('Digite o nome do canal:');
+  final c = canais.where((c) => c.nome == ).firstOrNull;
+
+  if (c == null) {
+    print('Canal não encontrado!');
+    return;
+  }
+  c.exibirMensagens();
+}
+
+
+void relatorio() {
+    print('DARTCORD');
+    print('Servidor: Programação Mobile');
+    print('Usuários online:');
+    for (final u in usuarios.where((u) => u.ativo)) {
+        print(u.nick);
+    }
+
+    print('Canais:');
+    for (final c in canais) {
+        print('#${c.nome}');
+    }
+    print('Exibindo mensagens do canal:');
+    exibirCanal();
+}
+
+void menu(){
+    while (true){
+        print('\nVocê entrou no DARTCORD');
+        print('1 - Criar usuário\n2 - Criar moderador\n 3 - Criar canal');
+        print('4 - Enviar mensagem\n 5 - Exibir canal\n6 - Relatório\n0 - Sair');
+
+        switch (int.tryParse(stdin.readLineSync() ?? '') ?? -1) {
+            case 1:
+                criarUsuario();
+                break;
+            case 2:
+                criarUsuario(moderador: true);
+                break;
+            case 3:
+                criarCanal();
+                break;
+            case 4:
+                enviarMensagem();
+                break;
+            case 5:
+                exibirCanal();
+                break;
+            case 6:
+                relatorio();
+                break;
+            case 0:
+                return;
+            default:
+                print('Opção inválida!');
+        }
+    }
+}
+
+void main() {
+  menu();
+}
