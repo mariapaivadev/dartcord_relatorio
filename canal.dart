1d74d5ba0892b33b29edfcf0148905e2842ef7a4
mariapaivadev <mariapaiva.dev@gmail.com>
2026-09-23 21:50:03 -0400

    Criacao class canal de mensagens

--- canal.dart
+++ canal.dart
@@ -6,3 +6,19 @@ class mensagem {
 
   mensagem({this.autor, this.texto});
 }
+
+class canal {
+  final String nome;
+  final List<mensagem> _mensagens = [];
+
+  canal(this.nome);
+
+  void adicionarMensagem(mensagem msg) => _mensagens.add(msg);
+
+  void exibirMensagens() {
+    print('#$nome');
+    for (final msg in _mensagens) {
+      print('${msg.autor.nick}: ${msg.texto}');
+    }
+  }
+}
