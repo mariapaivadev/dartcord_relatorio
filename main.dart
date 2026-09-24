8de72ecc09270d5021be9565600487bb9045b05d
mariapaivadev <mariapaiva.dev@gmail.com>
2026-09-23 22:31:50 -0400

    Correções

--- main.dart
+++ main.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'usuario.dart';
 import 'canal.dart';
 
-final usuarios = <usuario>[];
-final canais = <canal>[];
+final usuarios = <Usuario>[];
+final canais = <Canal>[];
 
-string ler(String pergunta) {
+String ler(String pergunta) {
   print(pergunta);
   return stdin.readLineSync() ?? '';
 }
@@ -17,14 +17,14 @@ void criarUsuario({bool moderador = false}) {
 
   usuarios.add(
     moderador
-        ? moderador(nick: nick, idade: idade)
-        : usuario(nick: nick, idade: idade),
+        ? Moderador(nick: nick, idade: idade)
+        : Usuario(nick: nick, idade: idade),
   );
   print('Criado com sucesso!');
 }
 
 void criarCanal() {
-  canais.add(canal(ler('Digite o nome do canal:')));
+  canais.add(Canal(ler('Digite o nome do canal:')));
   print('Criado com sucesso!');
 }
 
@@ -32,18 +32,18 @@ void enviarMensagem() {
   final nick = ler('Digite o nick do autor:');
   final nomeCanal = ler('Digite o nome do canal:');
   final u = usuarios.where((u) => u.nick == nick).firstOrNull;
-  final c = canais.where((c) => c.nome == nomeCanal).firstorNull;
+  final c = canais.where((c) => c.nome == nomeCanal).firstOrNull;
 
   if (u == null || c == null) {
     print('Usuário não encontrado!');
     return;
   }
-  c.adicionarMensagem(mensagem(u, ler('Mensagem:')));
+  c.adicionarMensagem(Mensagem(u, ler('Mensagem:')));
 }
 
 void exibirCanal() {
   final nome = ler('Digite o nome do canal:');
-  final c = canais.where((c) => c.nome == ).firstOrNull;
+  final c = canais.where((c) => c.nome == nome).firstOrNull;
 
   if (c == null) {
     print('Canal não encontrado!');
@@ -52,54 +52,53 @@ void exibirCanal() {
   c.exibirMensagens();
 }
 
-
 void relatorio() {
-    print('DARTCORD');
-    print('Servidor: Programação Mobile');
-    print('Usuários online:');
-    for (final u in usuarios.where((u) => u.ativo)) {
-        print(u.nick);
-    }
+  print('DARTCORD');
+  print('Servidor: Programação Mobile');
+  print('Usuários online:');
+  for (final u in usuarios.where((u) => u.ativo)) {
+    print(u.nick);
+  }
 
-    print('Canais:');
-    for (final c in canais) {
-        print('#${c.nome}');
-    }
-    print('Exibindo mensagens do canal:');
-    exibirCanal();
+  print('Canais:');
+  for (final c in canais) {
+    print('#${c.nome}');
+  }
+  print('Exibindo mensagens do canal:');
+  exibirCanal();
 }
 
-void menu(){
-    while (true){
-        print('\nVocê entrou no DARTCORD');
-        print('1 - Criar usuário\n2 - Criar moderador\n 3 - Criar canal');
-        print('4 - Enviar mensagem\n 5 - Exibir canal\n6 - Relatório\n0 - Sair');
+void menu() {
+  while (true) {
+    print('\nVocê entrou no DARTCORD');
+    print('1 - Criar usuário\n2 - Criar moderador\n 3 - Criar canal');
+    print('4 - Enviar mensagem\n 5 - Exibir canal\n6 - Relatório\n0 - Sair');
 
-        switch (int.tryParse(stdin.readLineSync() ?? '') ?? -1) {
-            case 1:
-                criarUsuario();
-                break;
-            case 2:
-                criarUsuario(moderador: true);
-                break;
-            case 3:
-                criarCanal();
-                break;
-            case 4:
-                enviarMensagem();
-                break;
-            case 5:
-                exibirCanal();
-                break;
-            case 6:
-                relatorio();
-                break;
-            case 0:
-                return;
-            default:
-                print('Opção inválida!');
-        }
+    switch (int.tryParse(stdin.readLineSync() ?? '') ?? -1) {
+      case 1:
+        criarUsuario();
+        break;
+      case 2:
+        criarUsuario(moderador: true);
+        break;
+      case 3:
+        criarCanal();
+        break;
+      case 4:
+        enviarMensagem();
+        break;
+      case 5:
+        exibirCanal();
+        break;
+      case 6:
+        relatorio();
+        break;
+      case 0:
+        return;
+      default:
+        print('Opção inválida!');
     }
+  }
 }
 
 void main() {
